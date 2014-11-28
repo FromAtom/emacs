89c56acaf12e66ab1d0a1c3adb2c3a65b1d997bc
Atom <fromatom@gmail.com>
2014-11-28 16:20:30 +0900

    language-envをJapaneseにはしない

--- .emacs.d/inits/00-init.el
+++ .emacs.d/inits/00-init.el
@@ -1,4 +1,5 @@
 ;; 文字コード設定
+(set-default-coding-systems 'utf-8-unix)
 (setq default-buffer-file-coding-system 'utf-8-unix)
 (set-buffer-file-coding-system 'utf-8-unix)
 (set-terminal-coding-system 'utf-8-unix)
@@ -6,9 +7,7 @@
 (set-clipboard-coding-system 'utf-8-unix)
 (prefer-coding-system 'utf-8-unix)
 (set-language-environment 'utf-8)
-(set-language-environment "Japanese")
 (setq locale-coding-system 'utf-8)
-(set-default-coding-systems 'utf-8-unix)
 
 ;; Max OS Xのファイルネーム設定
 (when (eq system-type 'darwin)
