452c6454d2435479eb123e3adbc921846e72e841
Atom <fromatom@gmail.com>
2015-01-11 12:00:20 +0900

    実行順を変更

--- .emacs.d/inits/00-init.el
+++ .emacs.d/inits/00-init.el
@@ -1,10 +1,10 @@
 ;; 文字コード設定
 (set-default-coding-systems 'utf-8-unix)
 (setq default-buffer-file-coding-system 'utf-8-unix)
-(set-buffer-file-coding-system 'utf-8-unix)
 (set-terminal-coding-system 'utf-8-unix)
 (set-keyboard-coding-system 'utf-8-unix)
 (set-clipboard-coding-system 'utf-8-unix)
+(set-buffer-file-coding-system 'utf-8-unix)
 (prefer-coding-system 'utf-8-unix)
 (set-language-environment 'utf-8)
 (setq locale-coding-system 'utf-8)
